9bfdf1e807ff5e28ff9d3e678066ccc05df9e251
Marcos  Borrillo <68borrismar@gmail.com>
2026-06-14 23:44:41 +0200

    Create test_queries.sql

---
+++ sql/load/test_queries.sql
@@ -0,0 +1,9 @@
+SELECT COUNT(*) AS boletines FROM boletines;
+
+SELECT COUNT(*) AS ofertas FROM ofertas;
+
+SELECT COUNT(*) AS competencias FROM competencias;
+
+SELECT COUNT(*) AS empleo_publico FROM empleo_publico;
+
+SELECT COUNT(*) AS formaciones FROM formaciones;
