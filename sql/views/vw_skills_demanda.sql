2a5097fcc28e891816171ed006b2c25195d02d92
Marcos  Borrillo <68borrismar@gmail.com>
2026-06-14 21:28:00 +0200

    Create vw_skills_demanda.sql

---
+++ sql/views/vw_skills_demanda.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE VIEW vw_skills_demanda AS
+SELECT
+
+    sk.skill_id,
+
+    sk.nombre AS skill,
+
+    COUNT(*) AS total_ofertas
+
+FROM oferta_skill os
+
+INNER JOIN skills sk
+    ON os.skill_id = sk.skill_id
+
+GROUP BY
+    sk.skill_id,
+    sk.nombre;
